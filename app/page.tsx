488e54ac270b12eb7d51e634d2103fea1cc0a12e
senzo-zwelihle-masango <senzo@vxpress.co.za>
2025-10-18 17:55:13 +0200

    shadcn components installed as well setup tailwind prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       Home
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
