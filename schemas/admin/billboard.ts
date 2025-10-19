8502944ad4ab765177c6a5372098e98185ddd1b1
senzo-zwelihle-masango <senzo@vxpress.co.za>
2025-10-19 14:13:47 +0200

    feature: billboards

---
+++ schemas/admin/billboard.ts
@@ -0,0 +1,15 @@
+import { z } from "zod"
+import { BillboardStatus } from "@/lib/generated/prisma"
+
+export const billboardSchema = z.object({
+  categoryId: z.string().min(1, "Category is required"),
+  label: z.string().max(30, "Label must be under 30 characters."),
+  description: z.string().max(30, "Label must be under 30 characters."),
+  image: z
+    .string()
+    .min(1, "Collection image is required, please upload atleast one image."),
+  status: z.enum(BillboardStatus),
+  featuredProductId: z.string().nullable().optional()
+})
+
+export type BillboardSchemaType = z.infer<typeof billboardSchema>
